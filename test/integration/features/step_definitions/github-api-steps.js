154de396829da471b4eda7dcee6308c852156dbe
Matt Travi <programmer@travi.org>
2020-07-12 00:51:57 -0500

    test(integration): backfilled coverage of core behavior

---
+++ test/integration/features/step_definitions/github-api-steps.js
@@ -0,0 +1,32 @@
+import nock from 'nock';
+import {OK} from 'http-status-codes';
+import {After, Before, Given, Then} from 'cucumber';
+import any from '@travi/any';
+import {assert} from 'chai';
+
+let githubScope;
+const user = any.simpleObject();
+
+Before(function () {
+  nock.disableNetConnect();
+
+  githubScope = nock('https://api.github.com/');
+});
+
+After(() => {
+  nock.enableNetConnect();
+  nock.cleanAll();
+});
+
+Given('a personal access token is defined for api.github.com', async function () {
+  this.personalAccessToken = any.string();
+
+  githubScope
+    .matchHeader('Authorization', `token ${this.personalAccessToken}`)
+    .get('/user')
+    .reply(OK, user);
+});
+
+Then('the user is returned', async function () {
+  assert.deepEqual(this.result.data, user);
+});
---
+++ test/integration/features/step_definitions/netrc-steps.js
@@ -0,0 +1,49 @@
+import {Octokit} from '@octokit/core';
+// eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
+import {createNetrcAuth} from 'octokit-auth-netrc';
+import {After, Given, Then, When} from 'cucumber';
+import stubbedFs from 'mock-fs';
+import {assert} from 'chai';
+
+let netrcExists;
+
+After(function () {
+  stubbedFs.restore();
+
+  netrcExists = null;
+});
+
+Given('no netrc file exists', async function () {
+  netrcExists = false;
+});
+
+Given('the netrc file exists', async function () {
+  netrcExists = true;
+});
+
+When('the user account is requested', async function () {
+  stubbedFs({
+    ...netrcExists && {
+      [`${process.env.HOME}/.netrc`]: this.personalAccessToken
+        ? `
+machine api.github.com
+  login ${(this.personalAccessToken)}
+`
+        : ''
+    }
+  });
+  const OctokitWithNetrcAuth = Octokit.defaults({authStrategy: createNetrcAuth});
+
+  try {
+    const octokit = new OctokitWithNetrcAuth();
+
+    this.result = await octokit.request('GET /user');
+  } catch (e) {
+    this.result = e;
+  }
+});
+
+Then('a missing-token error is thrown', async function () {
+  assert.equal(this.result.code, 'ENONETRCTOKEN');
+  assert.equal(this.result.message, 'No entry was found for `api.github.com` in your `~/.netrc` file');
+});
