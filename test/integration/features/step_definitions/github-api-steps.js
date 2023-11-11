78511e2d573e8c8344cb42355b9a0c667cbca05c
Matt Travi <programmer@travi.org>
2023-11-11 00:04:33 -0600

    test(fetch): swapped nock for fetch-mock in order to stub native fetch

--- test/integration/features/step_definitions/github-api-steps.js
+++ test/integration/features/step_definitions/github-api-steps.js
@@ -1,33 +1,24 @@
-import nock from 'nock';
+import fetchMock from 'fetch-mock';
 import {StatusCodes} from 'http-status-codes';
-import {After, Before, Given, Then} from '@cucumber/cucumber';
+import {After, Given, Then} from '@cucumber/cucumber';
 import any from '@travi/any';
 import {assert} from 'chai';
 
-let githubScope;
 const user = any.simpleObject();
 
-Before(function () {
-  nock.disableNetConnect();
-
-  githubScope = nock('https://api.github.com/');
-});
-
 After(() => {
-  nock.enableNetConnect();
-  nock.cleanAll();
-
-  assert.isTrue(githubScope.isDone());
+  fetchMock.reset();
 });
 
 Given('a personal access token is defined for {string}', async function (domain) {
   this.personalAccessToken = any.word();
-  githubScope = nock(`https://${domain}/`);
 
-  githubScope
-    .matchHeader('Authorization', `token ${this.personalAccessToken}`)
-    .get('/user')
-    .reply(StatusCodes.OK, user);
+  fetchMock
+    .getOnce(
+      `https://${domain}/user`,
+      {status: StatusCodes.OK, body: user},
+      {headers: {Authorization: `token ${this.personalAccessToken}`}}
+    );
 });
 
 Then('the user is returned', async function () {
